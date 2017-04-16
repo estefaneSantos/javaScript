d18dd527800ca71689e0437f67a70964d004de5f
estefaneSantos <estefanemsantos@hotmail.com>
2017-04-16 16:28:23 -0300

    Add files via upload

---
+++ exemplo14.1.js
@@ -0,0 +1,7 @@
+function evento_entrar() {
+       alert("seja bem vindo"); }
+       function evento_sair() {
+       alert("até a próxima visita");}
+       function evento_clique() {
+       window.document.getElementById('id2').style.color='red'; }
+       
